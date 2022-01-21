6bbd2c7bc6ab5b49603261d0c8275cab6a41351a
nasrulnoor <91359325+nasrulnoor@users.noreply.github.com>
2022-01-21 21:34:07 +0800

    Update frpcinstall.sh

--- frpcinstall.sh
+++ frpcinstall.sh
@@ -2,6 +2,7 @@
 wget https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
 tar -xf frp_0.38.0_linux_amd64.tar.gz
 cd frp_0.38.0_linux_amd64
+mkdir /etc/frp/
 cp frpc /usr/bin/frpc 
 cp frpc.ini /etc/frp/frpc.ini
 cp systemd/frpc.service /lib/systemd/system
