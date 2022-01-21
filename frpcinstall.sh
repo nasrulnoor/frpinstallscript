ea56fd0850d60d5e04fa35d1e9b56148209b0a41
nasrulnoor <91359325+nasrulnoor@users.noreply.github.com>
2022-01-21 21:29:01 +0800

    Create frpcinstall.sh

---
+++ frpcinstall.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+wget https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
+tar -xf frp_0.38.0_linux_amd64.tar.gz
+cd frp_0.38.0_linux_amd64
+cp frpc /usr/bin/frpc 
+cp frpc.ini /etc/frp/frpc.ini
+cp systemd/frpc.service /lib/systemd/system
+systemctl enable frpc.service
+systemctl start frpc
